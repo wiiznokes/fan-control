b0672c13966777ec21f8befd6bf2a1311cc578da
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-10-30 14:20:30 +0100

    experimental tick + ui

--- experimental/src/main.rs
+++ experimental/src/main.rs
@@ -1,37 +1,94 @@
-use lm_sensors::{value, LMSensors, SubFeatureRef};
-use ouroboros::self_referencing;
+#![allow(dead_code)]
 
-#[self_referencing]
-pub struct X {
-    lib: LMSensors,
-    #[borrows(lib)]
-    #[not_covariant]
-    v: Vec<SubFeatureRef<'this>>,
+use iced::widget::{Button, Column, Container, Row, Space, TextInput};
+use iced::{self, executor, widget::Text, Application, Command, Settings};
+use iced::{time, Length};
+
+use std::time::Duration;
+
+struct AppState {
+    // goal:
+    // - mutate and access in app
+    // - access in subscription
+    name: String,
+
+    // goal:
+    // - mutate and access in app
+    // - mutate and access in subscription
+    number: i32,
 }
 
-impl X {
-    pub fn contruct() -> Self {
-        XBuilder {
-            lib: lm_sensors::Initializer::default().initialize().unwrap(),
-            v_builder: |lib_from_struct: &LMSensors| {
-                let mut v = Vec::new();
+pub struct Ui {
+    app_state: AppState,
+}
 
-                for chip_ref in lib_from_struct.chip_iter(None) {
-                    for feature_ref in chip_ref.feature_iter() {
-                        if let Ok(sub_feature_ref) =
-                            feature_ref.sub_feature_by_kind(value::Kind::FanInput)
-                        {
-                            v.push(sub_feature_ref)
-                        }
-                    }
-                }
-                v
-            },
+#[derive(Debug, Clone)]
+pub enum AppMsg {
+    Tick,
+    ChangeName(String),
+    ChangeNumber(i32),
+}
+
+impl Application for Ui {
+    type Executor = executor::Default;
+    type Message = AppMsg;
+    type Theme = iced::Theme;
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
+        let app_state = AppState {
+            name: "name1".into(),
+            number: 0,
+        };
+
+        let ui_state = Ui { app_state };
+
+        (ui_state, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("App")
+    }
+
+    fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
+        match message {
+            AppMsg::Tick => {
+                println!("tick")
+            }
+            AppMsg::ChangeName(name) => self.app_state.name = name,
+            AppMsg::ChangeNumber(number) => self.app_state.number = number,
         }
-        .build()
+
+        Command::none()
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
+        let counter = Row::new()
+            .push(
+                Button::new(Text::new("increment"))
+                    .on_press(AppMsg::ChangeNumber(self.app_state.number + 1)),
+            )
+            .push(Space::new(16, 0))
+            .push(Text::new(self.app_state.number.to_string()));
+
+        let content = Column::new()
+            .push(
+                TextInput::new("", &self.app_state.name).on_input(|text| AppMsg::ChangeName(text)),
+            )
+            .push(Space::new(0, 30))
+            .push(counter)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .align_items(iced::Alignment::Center);
+
+        Container::new(content).into()
+    }
+
+    fn subscription(&self) -> iced::Subscription<Self::Message> {
+        time::every(Duration::from_millis(1000)).map(|_| AppMsg::Tick)
     }
 }
 
 fn main() {
-    let _x = X::contruct();
+    Ui::run(Settings::default()).unwrap();
 }
