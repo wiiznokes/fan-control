89f9bb535b45249169acb7d319910b32914fef67
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-12-02 22:48:32 +0100

    Update settings.rs

--- data/src/settings.rs
+++ data/src/settings.rs
@@ -2,12 +2,17 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Settings {
+
+    #[serde(default)]
     pub unit: Unit,
 
+    #[serde(default = "default_update_delay")]
     pub update_delay: u64,
 
+    #[serde(default = "default_disable_pwm_value")]
     pub disable_pwm_value: u8,
 
+    #[serde(default)]
     pub current_config: Option<String>,
 }
 
@@ -22,10 +27,17 @@ impl Default for Settings {
     fn default() -> Self {
         Self {
             unit: Default::default(),
-            update_delay: 1000,
-            disable_pwm_value: 2,
+            update_delay: default_update_delay(),
+            disable_pwm_value: default_disable_pwm_value(),
             current_config: Default::default(),
         }
     }
 }
 
+fn default_update_delay() -> u64 {
+    1000
+}
+
+fn default_disable_pwm_value() -> u8 {
+    2
+}
