bd7f927ae87edb05d8b39ad00a7c9ec13ca4ccd4
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-01-05 17:07:05 +0100

    add window icon for windows

---
+++ build.rs
@@ -0,0 +1,15 @@
+use std::{env, io};
+
+// https://github.com/mxre/winres/
+
+fn main() -> io::Result<()> {
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        if std::env::var("PROFILE").unwrap() == "release" {
+            winres::WindowsResource::new()
+            .set_icon("resource/app_icon/app_icon150.ico")
+            .set_manifest_file("resource/windows/manifest.xml")
+            .compile()?;
+        }
+    }
+    Ok(())
+}
