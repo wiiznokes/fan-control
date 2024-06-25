ed3bb8bc9892c4cef60c6eee1fcd5991d06c7287
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-06-25 22:20:12 +0200

    fix: graph logic

--- data/src/utils.rs
+++ data/src/utils.rs
@@ -1,5 +1,7 @@
 use std::cmp::Ordering;
 
+use std::collections::BTreeSet;
+
 pub trait RemoveElem<T> {
     fn remove_elem<F>(&mut self, predicate: F) -> Option<T>
     where
@@ -17,6 +19,36 @@ impl<T> RemoveElem<T> for Vec<T> {
     }
 }
 
+pub fn has_duplicate<T>(iter: T) -> bool
+where
+    T: IntoIterator,
+    T::Item: Ord,
+{
+    let mut uniq = BTreeSet::new();
+    !iter.into_iter().all(move |x| uniq.insert(x))
+}
+
+pub fn is_sorted<T>(iter: T) -> bool
+where
+    T: IntoIterator,
+    T::Item: Ord,
+{
+    let mut iter = iter.into_iter();
+
+    let mut prev = iter.next();
+
+    for current in iter {
+        if let Some(ref prev) = prev {
+            if prev > &current {
+                return false;
+            }
+        }
+        prev.replace(current);
+    }
+
+    true
+}
+
 #[cfg(test)]
 pub fn init_test_logging() {
     let _ = env_logger::builder()
@@ -26,6 +58,7 @@ pub fn init_test_logging() {
 }
 
 pub trait InsertSorted<T> {
+    /// Don't allow duplicate
     fn insert_sorted<F>(&mut self, predicate: F, element: T) -> Option<T>
     where
         F: Fn(&T) -> Ordering;
