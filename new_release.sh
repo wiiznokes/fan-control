98905d331c13126784e18e282fd19c3666f1a6d7
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-08-23 14:22:27 +0200

    new release !log

--- new_release.sh
+++ new_release.sh
@@ -25,8 +25,9 @@ git push
 
 gh release delete $NEW_VERSION -y || true
 git tag -d $NEW_VERSION || true
-git tag $NEW_VERSION || true
-git push origin --tags || true
+git push origin --delete $NEW_VERSION || true
+git tag $NEW_VERSION
+git push origin $NEW_VERSION
 
 changelog-gen show >RELEASE_CHANGELOG.md
 
