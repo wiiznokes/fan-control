27186836dbf7fb9ed024af16e43e476e18c91436
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2025-07-29 16:07:05 +0200

    improve add item column visibility

--- ui/src/add_node.rs
+++ ui/src/add_node.rs
@@ -1,15 +1,20 @@
 use cosmic::{
     Element,
-    iced_widget::{Column, text},
-    widget::tooltip,
+    iced::padding,
+    iced_widget::text,
+    widget::{column, tooltip},
 };
 use data::node::NodeTypeLight;
 
 use crate::{AppMsg, ToogleMsg, icon::icon_button_from_handle, icon_button, node_icon_handle};
 
+const ICON_SIZE: u16 = 25;
+
 pub fn add_node_button_view(expanded: bool) -> Element<'static, AppMsg> {
+    let column = column().padding(padding::bottom(15));
+
     match expanded {
-        true => Column::new()
+        true => column
             .push(add_item(NodeTypeLight::Control, fl!("add_control")))
             .push(add_item(NodeTypeLight::Fan, fl!("add_fan")))
             .push(add_item(NodeTypeLight::Temp, fl!("add_temp")))
@@ -18,21 +23,30 @@ pub fn add_node_button_view(expanded: bool) -> Element<'static, AppMsg> {
             .push(add_item(NodeTypeLight::Target, fl!("add_target")))
             .push(add_item(NodeTypeLight::Graph, fl!("add_graph")))
             .push(add_item(NodeTypeLight::Flat, fl!("add_flat")))
-            .push(icon_button!("close/40").on_press(AppMsg::Toggle(ToogleMsg::CreateButton(false))))
+            .push(
+                icon_button!("close/24")
+                    .on_press(AppMsg::Toggle(ToogleMsg::CreateButton(false)))
+                    .icon_size(ICON_SIZE),
+            )
             .into(),
 
-        false => tooltip(
-            icon_button!("add/40").on_press(AppMsg::Toggle(ToogleMsg::CreateButton(true))),
-            text(fl!("add_item")),
-            tooltip::Position::Top,
-        )
-        .into(),
+        false => column
+            .push(tooltip(
+                icon_button!("add/24")
+                    .on_press(AppMsg::Toggle(ToogleMsg::CreateButton(true)))
+                    .icon_size(ICON_SIZE),
+                text(fl!("add_item")),
+                tooltip::Position::Top,
+            ))
+            .into(),
     }
 }
 
 fn add_item<'a>(kind: NodeTypeLight, desc: String) -> Element<'a, AppMsg> {
     tooltip(
-        icon_button_from_handle(node_icon_handle!(kind)).on_press(AppMsg::NewNode(kind.clone())),
+        icon_button_from_handle(node_icon_handle!(kind))
+            .on_press(AppMsg::NewNode(kind.clone()))
+            .icon_size(ICON_SIZE),
         text(desc),
         tooltip::Position::Left,
     )
