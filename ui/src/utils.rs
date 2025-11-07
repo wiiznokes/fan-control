d3e3e7fc1932cd8b0a2a42940233da146b996d32
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2025-11-07 10:34:51 +0100

    fix text

--- ui/src/utils.rs
+++ ui/src/utils.rs
@@ -1,7 +1,7 @@
 #[cfg(target_os = "windows")]
 pub fn setup_wgpu() {
     unsafe {
-        std::env::set_var("WGPU_BACKEND", "gl");
+        std::env::set_var("ICED_BACKEND", "tiny-skia");
     }
     debug!("WGPU: Forcing OpenGL backend for graphics.");
 }
