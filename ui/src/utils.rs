a48c658552f3890ee4ad35eb304c0b0ccddc8230
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2025-11-08 14:25:41 +0100

    Update utils.rs

--- ui/src/utils.rs
+++ ui/src/utils.rs
@@ -1,9 +1,9 @@
 #[cfg(target_os = "windows")]
 pub fn setup_wgpu() {
-    unsafe {
-        std::env::set_var("ICED_BACKEND", "tiny-skia");
-    }
-    debug!("WGPU: Forcing OpenGL backend for graphics.");
+    // unsafe {
+    //     std::env::set_var("ICED_BACKEND", "tiny-skia");
+    // }
+    // debug!("WGPU: Forcing OpenGL backend for graphics.");
 }
 
 #[cfg(not(target_os = "windows"))]
